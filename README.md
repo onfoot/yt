33b6023c5ceb9f69a26bd8c0ef6cc09307363ca3
Maciej Rutkowski <onfoot@users.noreply.github.com>
2023-09-10 17:04:14 +0200

    chore: slim down docker images, add screenshot, clean up the code a bit

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # yt
-Simple, self-hosted web-based video downloader using the popular tool *wink wink*
+Simple, self-hosted web-based video downloader using the popular tool *wink wink*.
+
+![Screenshot](assets/screenshot.png)
 
 It attempts to download h264 videos from the popular online video sites so they're readily available to play using any browser without any conversion and workarounds.
--- app/app.py
+++ app/app.py
@@ -2,12 +2,13 @@ from flask import Flask, request, render_template, send_from_directory, redirect
 import yt_dlp
 import os
 
+VIDEO_EXTENSION=".mp4"
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     video_url = None
-    video_path = None
 
     if request.method == 'POST':
         video_url = request.form['video_url']
@@ -16,32 +17,33 @@ def index():
     
     video_files = list_mp4_video_files()
     formatted_video_files = [
-        {'original_filename': filename, 'sanitized_title': format_filename(filename)} for filename in video_files
+        {
+            'original_filename': filename, 
+            'sanitized_title': format_filename(filename)
+        } for filename in video_files
     ]
     return render_template('index.html', video_files=formatted_video_files)
 
 def list_mp4_video_files():
-    video_files = [f for f in os.listdir('videos') if f.endswith('.mp4')]
+    video_files = [f for f in os.listdir('videos') if f.endswith(VIDEO_EXTENSION)]
     return video_files
 
 def format_filename(filename):
-    # Remove the '.mp4' extension and replace underscores with spaces
-    return filename.replace('_', ' ').replace('.mp4', '')
+    return filename.replace('_', ' ').replace(VIDEO_EXTENSION, '')
 
 
 def download_video(url):
     ydl_opts = {
         'outtmpl': 'videos/%(id)s.%(ext)s',
         'format': 'bv[ext=mp4][vcodec~=\'^((he|a)vc|h26[45])\']+ba[ext=m4a]',
-        #'format_sort': {'codec': 'h264'}
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(url, download=False)
         video_id = info_dict.get('id', 'video')
         video_title = info_dict.get('title', 'video')
-        video_filename = f'{video_id}.mp4'
-        safe_filename = f'{video_title}.mp4'.replace(':', '_').replace('?', '_').replace(' ', '_')
+        video_filename = f'{video_id}{VIDEO_EXTENSION}'
+        safe_filename = f'{video_title}{VIDEO_EXTENSION}'.replace(':', '_').replace('?', '_').replace(' ', '_')
 
         ydl.download([url])
 
@@ -56,5 +58,6 @@ def download(filename):
 if __name__ == '__main__':
     if not os.path.exists('videos'):
         os.makedirs('videos')
-    app.run(host='0.0.0.0', port=4999, debug=True)
+
+    app.run(host='0.0.0.0', port=4999, debug=False)
 
