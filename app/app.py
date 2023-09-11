b4f05bee8246502c1a7ea44dfb8942238305b466
Maciej Rutkowski <onfoot@users.noreply.github.com>
2023-09-11 21:03:19 +0200

    fix: fetch and embed video metadata, chapters, and subtitles; sort videos by download time

--- app/app.py
+++ app/app.py
@@ -2,9 +2,10 @@ from flask import Flask, request, render_template, send_from_directory, redirect
 import yt_dlp
 import os
 
-VIDEO_EXTENSION=".mp4"
+VIDEO_EXTENSION = ".mp4"
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -14,19 +15,24 @@ def index():
         video_url = request.form['video_url']
         download_video(video_url)
         return redirect('/')
-    
+
     video_files = list_mp4_video_files()
     formatted_video_files = [
         {
-            'original_filename': filename, 
+            'original_filename': filename,
             'sanitized_title': format_filename(filename)
         } for filename in video_files
     ]
     return render_template('index.html', video_files=formatted_video_files)
 
+
 def list_mp4_video_files():
-    video_files = [f for f in os.listdir('videos') if f.endswith(VIDEO_EXTENSION)]
-    return video_files
+    files = [f for f in os.listdir('videos') if f.endswith(VIDEO_EXTENSION)]
+    update_times = [os.path.getmtime(os.path.join('videos', f)) for f in files]
+    files = [f for _, f in sorted(zip(update_times, files), reverse=True)]
+
+    return files
+
 
 def format_filename(filename):
     return filename.replace('_', ' ').replace(VIDEO_EXTENSION, '')
@@ -36,11 +42,29 @@ def download_video(url):
     ydl_opts = {
         'outtmpl': 'videos/%(id)s.%(ext)s',
         'format': 'bv[ext=mp4][vcodec~=\'^((he|a)vc|h26[45])\']+ba[ext=m4a]',
-        'embed-metadata': True,
-        'embed-chapters': True,
-        'embed-subs': True,
-        'sub-format': 'ass',
-        'embed-thumbnail': True,
+        'postprocessors': [
+            {
+                'already_have_subtitle': False,
+                'key': 'FFmpegEmbedSubtitle'
+            },
+            {
+                'add_chapters': True,
+                'add_infojson': 'if_exists',
+                'add_metadata': True,
+                'key': 'FFmpegMetadata'},
+            {
+                'already_have_thumbnail': False, 'key': 'EmbedThumbnail'
+            },
+            {
+                'key': 'FFmpegConcat',
+                'only_multi_video': True,
+                'when': 'playlist'
+            }
+        ],
+        'subtitlesformat': 'ass',
+        'writesubtitles': True,
+        'writethumbnail': True,
+        'updatetime': False,
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -55,6 +79,7 @@ def download_video(url):
         os.rename('videos/' + video_filename, 'videos/' + safe_filename)
 
     return safe_filename
+
 
 @app.route('/videos/<path:filename>', methods=['GET'])
 def download(filename):
@@ -72,4 +97,3 @@ if __name__ == '__main__':
         os.makedirs('videos')
 
     app.run(host='0.0.0.0', port=4999, debug=True)
-
